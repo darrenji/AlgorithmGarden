26fb63f2a461ffda9447f987336d8c8b0fc3c9d7
darren <764190362@qq.com>
2018-11-04 20:33:17 +0800

    冒泡排序

---
+++ AlgorithmGarden.BubbleSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AlgorithmGarden.BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 5, 3, 12, 8 };
+            BubbleSort(arr);
+            foreach(var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        static void BubbleSort(int[] input)
+        {
+            bool hasSwapped;
+            do
+            {
+                hasSwapped = false;
+                for(var i=0; i<input.Length-1;i++)
+                {
+                    if(input[i] > input[i+1])
+                    {
+                        var oldValue = input[i + 1];
+                        input[i + 1] = input[i];
+                        input[i] = oldValue;
+                        hasSwapped = true;
+                    }
+                }
+            } while (hasSwapped);
+        }
+    }
+}
