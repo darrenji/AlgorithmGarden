fcba0f5a6923171b73eebabfd62a7bd4136e2c72
darren <764190362@qq.com>
2018-11-28 11:54:06 +0800

    字符串、long和时间类型转换

---
+++ AlgorithmGarden.Algorithms/Common/TimeConverHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmGarden.Algorithms.Common
+{
+    public class TimeConverHelper
+    {
+        public static long ConvertToLong(DateTime time)
+        {
+            return time.Ticks;
+        }
+
+        public static DateTime ConvertToTimeFromLong(long ticks) 
+        {
+            return new DateTime(ticks);
+        }
+
+        public static DateTime ConvertToTimeFromStr(string str)
+        {
+            DateTime now;
+            if(DateTime.TryParse(str, out now))
+            {
+                return now;
+            }
+            else
+            {
+                return DateTime.Now;
+            }
+        }
+    }
+}
--- AlgorithmGarden.Client/Program.cs
+++ AlgorithmGarden.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlgorithmGarden.Algorithms.Common;
+using System;
 
 namespace AlgorithmGarden.Client
 {
@@ -6,7 +7,16 @@ namespace AlgorithmGarden.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region 时间转换
+            string nowStr = "2018-11-28 10:29:22";
+            DateTime now = TimeConverHelper.ConvertToTimeFromStr(nowStr);
+
+            long nowLong = TimeConverHelper.ConvertToLong(now);
+            Console.WriteLine($"当前时间{nowStr}转换成long类型：{nowLong}");
+
+            DateTime newNow = TimeConverHelper.ConvertToTimeFromLong(nowLong);
+            Console.WriteLine($"当前long类型{nowLong}转换成时间：{newNow}");
+            #endregion
         }
     }
 }
