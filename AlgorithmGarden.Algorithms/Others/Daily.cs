256496259e3b8112c1c9e2174d216486acd14862
darren <764190362@qq.com>
2018-12-19 16:34:00 +0800

    为什么井盖是圆形的

---
+++ AlgorithmGarden.Algorithms/Others/Daily.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmGarden.Algorithms.Others
+{
+   /*
+   为什么井盖是圆形的？
+
+   保证不掉下去。圆形的直径一样，只要保证直径大于洞口直径，就不会掉下去。而方形，如果沿着对角线的方向竖起来，再稍稍倾斜，容易掉下去。
+
+   从材料成本。同样的外径，圆形用料更少。
+
+   从运输。圆形可以滚动，方便运输。
+   */
+}
